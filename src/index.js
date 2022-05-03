73827d2e104e618406cafbee27e6f554d2b5f107
humanless2u <humanless2u@protonmail.com>
2022-05-02 23:09:38 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
---
+++ src/pages/landpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Landpage(){ 
+    return (
+        <div className="container">
+            <h1>Hello World !</h1>
+        </div>
+      );
+}
